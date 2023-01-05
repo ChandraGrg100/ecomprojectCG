d2c8274bc08785d0638204b7fea4c7e82838850f
ChandraGrg100 <chandragurung11@gmail.com>
2023-01-05 21:41:55 +0545

    Index

--- eCom/eCom/settings.py
+++ eCom/eCom/settings.py
@@ -111,8 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",]
+    BASE_DIR/ "static"
+]
 
 
 # Default primary key field type
