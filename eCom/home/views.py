0c23e99f834ae6b089afc905067704d74acc6f78
ChandraGrg100 <chandragurung11@gmail.com>
2023-01-10 21:56:19 +0545

    Index

--- eCom/home/views.py
+++ eCom/home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
-
+def base(request):
+    return render(request, "base.html")
 
 def Home(request):
+
     return render(request, "index.html")
